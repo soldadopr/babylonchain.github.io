297caf599d2aa564ea5caed645c1cee98472d40a
soldadopr <161763094+soldadopr@users.noreply.github.com>
2024-08-18 07:26:38 +0100

    Update index.js

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -9,90 +9,62 @@ import eight from '../../../static/img/restapi.gif';
 import nine from '../../../static/img/supp.gif';
 import eleven from '../../../static/img/cli.gif';
 
-const FeatureList = [
+// Feature data with consistent keys and descriptive titles
+const featureList = [
   {
     url: 'docs/introduction/overview',
     title: 'Introduction',
     image: one,
-    description: (
-      <> 
-        Uncover the secrets of how Babylon operates.
-      </>
-    ),
+    description: 'Uncover the secrets of how Babylon operates.',
   },
   {
     url: 'docs/user-guides/installation',
     title: 'Installation Guide',
     image: two,
-    description: (
-      <>
-        Find out how to kick off your Babylon journey here.
-      </>
-    ),
+    description: 'Find out how to kick off your Babylon journey here.',
   },
   {
     url: 'docs/developer-guides/modules/overview',
     title: 'Modules',
     image: four,
-    description: (
-      <>
-        Discover the modules we offer.
-      </>
-    ),
+    description: 'Discover the modules we offer.',
   },
   {
     url: 'docs/user-guides/btc-staking-testnet/overview',
     title: 'Testnet',
     image: six,
-    description: (
-      <>
-        Use the Testnet resources to enhance your Babylon understanding.
-      </>
-    ),
+    description: 'Use the Testnet resources to enhance your Babylon understanding.',
   },
   {
     url: 'docs/developer-guides/grpcrestapi',
     title: 'gRPC Gateway (REST API)',
     image: eight,
-    description: (
-      <>
-        Have a look at our Rest API components!
-      </>
-    ),
+    description: 'Have a look at our REST API components!',
   },
   {
     url: 'docs/support',
     title: 'Support',
     image: nine,
-    description: (
-      <>
-        Get the support you need to succeed with our dedicated team of experts!
-      </>
-    ),
+    description: 'Get the support you need to succeed with our dedicated team of experts!',
   },
   {
     url: 'docs/cli/babylond/cli',
     title: 'CLI Command',
     image: eleven,
-    description: (
-      <>
-        A detailed guide on CLI commands.
-      </>
-    ),
+    description: 'A detailed guide on CLI commands.',
   },
 ];
 
-function Feature({image, title, description, url}) {
+function Feature({ image, title, description, url }) {
   return (
-    <a className='urlstyle' href={url}>
-        <div className="text--center">
-          {/* <Svg className={styles.featureSvg} role="img" /> */}
-          <img src={image}/>
-        </div>
-        <div>
-          <h3>{title}</h3>
-          <p>{description}</p>
-        </div>
+    <a className={styles.urlStyle} href={url}>
+      <div className="text--center">
+        <img src={image} alt={title} className={styles.featureImage} />
+      </div>
+      <div>
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </div>
     </a>
   );
 }
@@ -102,7 +74,7 @@ export default function HomepageFeatures() {
     <section className={styles.features}>
       <div className="container">
         <div className="row center-homepage">
-          {FeatureList.map((props, idx) => (
+          {featureList.map((props, idx) => (
             <Feature key={idx} {...props} />
           ))}
         </div>
